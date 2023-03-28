ed295d85ce47b593dcd892cc7087e3203a496bb6
cprice2work <93597633+cprice2work@users.noreply.github.com>
2023-03-27 23:06:46 -0400

    fixed

--- R3F-Website/src/components/canvas/Computers.jsx
+++ R3F-Website/src/components/canvas/Computers.jsx
@@ -13,10 +13,19 @@ const Computers = () => {
       <hemisphereLight intensity={0.15}
       groundcolor="black"/>
       <pointLight intensity={1}/>
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024} 
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
         position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
@@ -26,12 +35,12 @@ const ComputersCanvas = () => {
 
   return(
     <Canvas 
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense >
         <OrbitControls 
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
@@ -46,4 +55,4 @@ const ComputersCanvas = () => {
 
 
 
-export default Computers
+export default ComputersCanvas
